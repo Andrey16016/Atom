29d85853c7487d7ba28fcdf24752b2446375628d
Andrey16016 <dm0326342@gmail.com>
2025-08-06 14:12:36 +0500

    Add files via upload

---
+++ update.py
@@ -0,0 +1,18 @@
+print ("Запуск обновления LKMOD")
+
+import os
+import time
+
+
+os.system("clear")
+os.system("rm -rf LKMOD")
+os.system("git clone https://github.com/Andrey16016/LKMOD")
+print ("Обновлено")
+time.sleep(1)
+os.system("clear")
+exit()
+
+
+
+
+
